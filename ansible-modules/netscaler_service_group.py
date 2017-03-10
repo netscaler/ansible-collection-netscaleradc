0b57cf3ccf5322c15943baeca3daf61c3140063c
George Nikolopoulos <giorgos.nikolopoulos@citrix.com>
2017-03-10 17:37:39 +0200

    Initial try at service group module

--- ansible-modules/netscaler_service_group.py
+++ ansible-modules/netscaler_service_group.py
@@ -42,19 +42,53 @@ config_updated:
 
 from ansible.module_utils.basic import AnsibleModule
 
-# TODO
-# Actual implementation of the module goes here
 
-
-# TODO add actual module instantiation code
 def main():
-    module = AnsibleModule(
-        argument_spec=dict(
-            nsip=dict(required=True),
-        )
+    from nssrc.com.citrix.netscaler.nitro.resource.config.lb.lbvserver_servicegroup_binding import lbvserver_servicegroup_binding
+    from nssrc.com.citrix.netscaler.nitro.service.nitro_service import nitro_service
+    from nssrc.com.citrix.netscaler.nitro.exception.nitro_exception import nitro_exception
+    import netscaler
+
+    module_specific_arguments = dict(
+        weight=dict(type='float'),
+        name=dict(type='str'),
+        servicename=dict(type='str'),
+        servicegroupname=dict(type='str'),
     )
 
-    module.exit_json(config_updated=True)
+    argument_spec = dict()
+
+    argument_spec.update(netscaler.netscaler_common_arguments)
+
+    argument_spec.update(module_specific_arguments)
+
+    module = AnsibleModule(argument_spec=argument_spec)
+
+    # do the login stuff
+    client = nitro_service(module.params['nsip'], module.params['nitro_protocol'])
+    client.set_credential(module.params['nitro_user'], module.params['nitro_pass'])
+    client.timeout = float(module.params['nitro_timeout'])
+    client.certvalidation = module.params['ssl_cert_validation']
+    client.login()
+
+    actual = lbvserver_servicegroup_binding()
+    lbvserver_servicegroup_binding_proxy = netscaler.ConfigProxy(actual, client, module_specific_arguments, module)
+    try:
+        if module.params['state'] == 'enabled':
+            lbvserver_servicegroup_binding_proxy.add()
+            print('I did it')
+        elif module.params['state'] == 'disabled':
+            lbvserver_servicegroup_binding_proxy.delete()
+            print('I did it')
+    except nitro_exception as e:
+        msg = "nitro exception errorcode=" + str(e.errorcode) + ",message=" + e.message
+        module.fail_json(msg=msg)
+
+    # save and exit
+    client.save_config()
+    client.logout()
+
+    module.exit_json(failed=False)
 
 if __name__ == "__main__":
     main()
