1508b21fbfcd48d31c10d9eabb210b415cf29077
George Nikolopoulos <giorgos.nikolopoulos@citrix.com>
2017-06-07 18:38:56 +0300

    Enhance install script to include integration test

--- install.py
+++ install.py
@@ -128,6 +128,8 @@ def main():
 
         local_units_dir = os.path.join(here, 'test', 'units')
         for file in os.listdir(local_units_dir):
+            if not file.endswith('.py'):
+                continue
             print('Copying unit test file %s' % file)
             shutil.copy(
                 src=os.path.join(local_units_dir, file),
@@ -135,6 +137,24 @@ def main():
             )
     else:
         print('Could not find units tests dir')
+
+    integration_tests_dir = os.path.join(ansible_path, '..', '..', 'test', 'integration')
+    if os.path.exists(integration_tests_dir):
+        print('Found integration tests dir %s' % integration_tests_dir)
+        # copy netscaler yaml and everything under roles
+        shutil.copy(os.path.join(here, 'test', 'integration', 'netscaler.yaml'), integration_tests_dir)
+        roles_dir = os.path.join(here, 'test', 'integration', 'roles')
+        for item in os.listdir(roles_dir):
+            full_item = os.path.join(roles_dir, item)
+            target_item = os.path.join(integration_tests_dir, 'roles', item)
+            if os.path.isdir(full_item):
+                if os.path.exists(target_item):
+                    print('Deleting existing role dir %s' % target_item)
+                    shutil.rmtree(target_item)
+                print('Copying roles dir %s' % full_item)
+                shutil.copytree(full_item, target_item)
+    else:
+        print('Could not find integration tests dir')
 
 
 if __name__ == '__main__':
