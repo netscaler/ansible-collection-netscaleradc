18e5cf6af497b1556f9f215243ab841a5341c3bd
George Nikolopoulos <giorgos.nikolopoulos@citrix.com>
2017-06-09 14:17:17 +0300

    Saving changes to utils/scrap.py

--- utils/scrap.py
+++ utils/scrap.py
@@ -30,8 +30,8 @@ def scrap_page(page):
             continue
         entry['name'] = items[0].text_content().strip()
         type_text = items[1].text_content().strip()
-        type_text = re.sub('^<','',type_text)
-        type_text = re.sub('>$','',type_text)
+        type_text = re.sub('^<', '', type_text)
+        type_text = re.sub('>$', '', type_text)
 
         # Will throw KeyError if type text is not found in dict
         entry['type'] = {
@@ -66,25 +66,26 @@ def scrap_page(page):
         for line in description_lines:
             if line.startswith('Possible values'):
                 choices = []
-                values_str = re.sub('Possible values = ','', line)
-                choices = [ val.strip() for val in values_str.split(',') ]
+                values_str = re.sub('Possible values = ', '', line)
+                choices = [val.strip() for val in values_str.split(',')]
                 entry['choices'] = choices
         properties.append(entry)
     return properties
+
 
 def update_for_immutables(properties, base_command_url, item):
     m = re.match(r'^.*/(.*)$', item)
     if m is None:
         raise Exception('Cannot match item: ' % item)
-    command_with_dashes = m.group(1)
-    command_with_spaces = m.group(1).replace('-',' ')
+    # command_with_dashes = m.group(1)
+    command_with_spaces = m.group(1).replace('-', ' ')
 
     page = base_command_url + item + '.html'
 
     r = requests.get(page)
     htmltree = html.fromstring(r.content)
-    id = 'set-%s' % command_with_dashes
-    h2s = htmltree.xpath('''//h2/following-sibling::p''' )
+    # id = 'set-%s' % command_with_dashes
+    h2s = htmltree.xpath('''//h2/following-sibling::p''')
     for h2 in h2s:
         if h2.text is not None:
             doc_text = h2.text.strip().lower()
@@ -92,56 +93,53 @@ def update_for_immutables(properties, base_command_url, item):
                 break
     else:
         raise Exception('Cannot find "set %s"' % command_with_spaces)
-    #print(doc_text)
-    mutable_options = [ item[1:] for item in re.findall(r'-\w+', doc_text)]
-    #print('Mutables %s' % mutable_options)
+    # print(doc_text)
+    mutable_options = [item[1:] for item in re.findall(r'-\w+', doc_text)]
+    # print('Mutables %s' % mutable_options)
     updated_properties = []
     for property in properties:
-        #print('Muting property %s' % property['name'])
+        # print('Muting property %s' % property['name'])
         property['mutable'] = property['name'] in mutable_options
-        #print('new property %s' % property)
+        # print('new property %s' % property)
         updated_properties.append(property)
     return updated_properties
-    #print('options: %s' % [ item[1:] for item in re.findall(r'-\w+', doc_text)])
-    
+    # print('options: %s' % [ item[1:] for item in re.findall(r'-\w+', doc_text)])
 
 
 def main():
     base_nitro_url = 'http://docs.citrix.com/en-us/netscaler/11-1/nitro-api/nitro-rest/api-reference/configuration/'
     base_command_url = 'http://docs.citrix.com/en-us/netscaler/11-1/reference/netscaler-command-reference/'
     pages = [
-        #('basic/servicegroup', 'basic/servicegroup'),
-        #('basic/service', 'basic/service'),
-        #('basic/server', 'basic/server'),
-        #('basic/servicegroup_servicegroupmember_binding', None)
-        #('load-balancing/lbvserver', 'lb/lb-vserver'),
-        #('load-balancing/lbvserver_service_binding', None)
-        #('load-balancing/lbvserver_servicegroup_binding', None)
-        #('load-balancing/lbmonitor', 'lb/lb-monitor'),
-        #('content-switching/csvserver', 'cs/cs-vserver'),
-        #('content-switching/cspolicy', 'cs/cs-policy'),
-        #('content-switching/csaction', 'cs/cs-action'),
-        #('ssl/sslcertkey', 'ssl/ssl-certkey'),
-        #('global-server-load-balancing/gslbsite', 'gslb/gslb-site'),
-        #('global-server-load-balancing/gslbservice', 'gslb/gslb-service'),
-        #('global-server-load-balancing/gslbvserver', 'gslb/gslb-vserver'),
+        # ('basic/servicegroup', 'basic/servicegroup'),
+        # ('basic/service', 'basic/service'),
+        # ('basic/server', 'basic/server'),
+        # ('basic/servicegroup_servicegroupmember_binding', None)
+        # ('load-balancing/lbvserver', 'lb/lb-vserver'),
+        # ('load-balancing/lbvserver_service_binding', None)
+        # ('load-balancing/lbvserver_servicegroup_binding', None)
+        # ('load-balancing/lbmonitor', 'lb/lb-monitor'),
+        # ('content-switching/csvserver', 'cs/cs-vserver'),
+        # ('content-switching/cspolicy', 'cs/cs-policy'),
+        # ('content-switching/csaction', 'cs/cs-action'),
+        # ('ssl/sslcertkey', 'ssl/ssl-certkey'),
+        # ('global-server-load-balancing/gslbsite', 'gslb/gslb-site'),
+        # ('global-server-load-balancing/gslbservice', 'gslb/gslb-service'),
+        # ('global-server-load-balancing/gslbvserver', 'gslb/gslb-vserver'),
         ('global-server-load-balancing/gslbvserver_domain_binding', None),
 
     ]
     for page in pages:
-        page_file = re.sub('/','_',page[0])
+        page_file = re.sub('/', '_', page[0])
         page_file += '.json'
         if os.path.exists(page_file):
             print('Skipping %s' % page_file)
             continue
         properties = scrap_page(base_nitro_url + page[0] + '.html')
         if page[1] is not None:
-            properties = update_for_immutables(properties, base_command_url, page[1] )
+            properties = update_for_immutables(properties, base_command_url, page[1])
         print('writing to file %s' % page_file)
         with open(page_file, 'w') as fh:
             json.dump(properties, fh, indent=4)
-
-
 
 
 if __name__ == '__main__':
