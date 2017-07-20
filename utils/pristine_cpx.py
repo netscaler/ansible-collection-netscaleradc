a2298b4038270c9905687a8a459ccc96af201a9e
George Nikolopoulos <giorgos.nikolopoulos@citrix.com>
2017-07-20 14:19:23 +0300

    Update pristine_cpx.py to default to CPX 12.0

--- utils/pristine_cpx.py
+++ utils/pristine_cpx.py
@@ -4,31 +4,34 @@ import subprocess
 import time
 import paramiko
 import sys
+import os.path
 
-if len(sys.argv) == 1 or sys.argv[1] == '111':
-    DOCKER_NAME = 'test_fixture_111'
-    DOCKER_NET = 'mynet'
-    DOCKER_IP = '172.18.0.2'
-    DOCKER_IMAGE= 'cpx:11.1-48.10'
-    DOCKER_EXTRA=''
-elif sys.argv[1] == '120':
+if len(sys.argv) == 1 or sys.argv[1] == '120':
     DOCKER_NAME = 'test_fixture_120'
     DOCKER_NET = 'mynet'
     DOCKER_IP = '172.18.0.200'
-    DOCKER_IMAGE= 'cpx:12.0-41.22'
-    DOCKER_EXTRA=''
+    DOCKER_IMAGE = 'cpx:12.0-41.22'
+    DOCKER_EXTRA = ''
+elif sys.argv[1] == '111':
+    DOCKER_NAME = 'test_fixture_111'
+    DOCKER_NET = 'mynet'
+    DOCKER_IP = '172.18.0.2'
+    DOCKER_IMAGE = 'cpx:11.1-48.10'
+    DOCKER_EXTRA = ''
 
-output = subprocess.check_output('docker ps -a -f name=%s' % DOCKER_NAME, shell=True)
-# print('output %s' % len(output.split('\n')))
-if len(str(output).split('\n')) != 2:
+cmd = 'docker ps -a --filter name=%s' % DOCKER_NAME
+print('cmd: %s' % cmd)
+output = subprocess.check_output(cmd, shell=True)
+print('output len %s' % len(str(output).split('\n')))
+if len(str(output).split('\n')) != 1:
     print('Removing existing container')
     subprocess.check_call('docker stop %s' % DOCKER_NAME, shell=True)
     subprocess.check_call('docker rm %s' % DOCKER_NAME, shell=True)
 
-if len(sys.argv) == 1 or sys.argv[1] == '111':
-    subprocess.check_call('docker run -dt -p 22 -p 80 -p 161/udp -e EULA-yes --ulimit core=-1 --privileged=true --name %s --net %s  --ip %s  %s ' % (DOCKER_NAME, DOCKER_NET, DOCKER_IP, DOCKER_IMAGE), shell=True)
-elif sys.argv[1] == '120':
+if len(sys.argv) == 1 or sys.argv[1] == '120':
     subprocess.check_call('docker run -dt -p 22 -p 80 -p 161/udp -e EULA-yes --ulimit core=-1 -v /var/cpx:/cpx --cap-add=NET_ADMIN --name %s --net %s  --ip %s  %s ' % (DOCKER_NAME, DOCKER_NET, DOCKER_IP, DOCKER_IMAGE), shell=True)
+elif sys.argv[1] == '111':
+    subprocess.check_call('docker run -dt -p 22 -p 80 -p 161/udp -e EULA-yes --ulimit core=-1 --privileged=true --name %s --net %s  --ip %s  %s ' % (DOCKER_NAME, DOCKER_NET, DOCKER_IP, DOCKER_IMAGE), shell=True)
 time.sleep(10)
 
 
@@ -52,3 +55,10 @@ print('stderr lines %s' % stderr.readlines())
 stdin, stdout, stderr = ssh.exec_command('/var/netscaler/bins/cli_script.sh "add dnsprofile dns-profile-1"')
 print('stdout lines %s' % stdout.readlines())
 print('stderr lines %s' % stderr.readlines())
+
+transport = paramiko.Transport(DOCKER_IP, 22)
+transport.connect(username='root', password='linux')
+sftp = paramiko.SFTPClient.from_transport(transport)
+here = os.path.dirname(os.path.realpath(__file__))
+for file in ['server.crt', 'server.key', 'server2.key', 'server2.crt']:
+    sftp.put(os.path.join(here, 'datafiles', file), os.path.join('/nsconfig/ssl', file))
