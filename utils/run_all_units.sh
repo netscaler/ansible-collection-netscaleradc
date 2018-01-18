2bbe5f46570c6fbb9f1fed4a0db934de7d90f4bb
George Nikolopoulos <giorgos.nikolopoulos@citrix.com>
2018-01-18 18:46:47 +0200

    Add utility script

---
+++ utils/run_all_units.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+set -e
+
+modules=( \
+netscaler_cs_action \
+netscaler_cs_policy \
+netscaler_cs_vserver \
+netscaler_gslb_service \
+netscaler_gslb_site \
+netscaler_gslb_vserver \
+netscaler_lb_monitor \
+netscaler_lb_vserver \
+netscaler_nitro_request \
+netscaler_save_config \
+netscaler_server \
+netscaler_servicegroup \
+netscaler_service \
+netscaler_ssl_certkey \
+)
+
+HERE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+# Run unit tests
+pversions=("2.6" "2.7" "3.5" "3.6")
+for module in "${modules[@]}" ; do
+    ${HERE_DIR}/run_units.sh $module
+done
