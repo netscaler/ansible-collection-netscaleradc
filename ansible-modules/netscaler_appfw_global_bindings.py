40f47ec6c0fdb714cafbecaf02aea34738deb950
George Nikolopoulos <giorgos.nikolopoulos@citrix.com>
2018-10-25 18:19:20 +0300

    Add appfw_global_bindings files

---
+++ ansible-modules/netscaler_appfw_global_bindings.py
@@ -0,0 +1,374 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+#  Copyright (c) 2018 Citrix Systems
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+from __future__ import absolute_import, division, print_function
+__metaclass__ = type
+
+
+ANSIBLE_METADATA = {'metadata_version': '1.1',
+                    'status': ['preview'],
+                    'supported_by': 'community'}
+
+
+
+DOCUMENTATION = '''
+---
+module: netscaler_appfw_global_bindings
+short_description: Define global bindings for AppFW
+description: 
+    - Define global bindings for AppFW
+    - Note that due to limitations in the NITRO API this module will always report a changed status.
+
+version_added: "2.8.0"
+
+options:
+
+
+
+    appfwpolicy_bindings:
+        desription: 
+        suboptions:
+            mode:
+                description:
+                    - If mode is C(bind):
+                    - Any bindings in the attributes list that do not exist will be created on the target Netscaler.
+                    - Existing bindings that are not on the attributes list remain unaffected.
+                    - If mode is C(unbind):
+                    - Any bindings defined in the attributes list that also exist on the target Netscaler will be removed.
+                    - Existing bindings that are not on the attributes list remain unaffected.
+                choices:
+                    - bind
+                    - unbind
+            attributes:
+                description: 
+                    - List of the attributes dictionaries for the bindings.
+                    - Valid attribute keys:
+                    - policyname
+                    - priority
+                    - gotopriorityexpression
+                    - invoke
+                    - state
+                    - labeltype
+                    - labelname
+                    - type
+                    - globalbindtype
+                    
+
+    auditnslogpolicy_bindings:
+        desription: 
+        suboptions:
+            mode:
+                description:
+                    - If mode is C(bind):
+                    - Any bindings in the attributes list that do not exist will be created on the target Netscaler.
+                    - Existing bindings that are not on the attributes list remain unaffected.
+                    - If mode is C(unbind):
+                    - Any bindings defined in the attributes list that also exist on the target Netscaler will be removed.
+                    - Existing bindings that are not on the attributes list remain unaffected.
+                choices:
+                    - bind
+                    - unbind
+            attributes:
+                description: 
+                    - List of the attributes dictionaries for the bindings.
+                    - Valid attribute keys:
+                    - policyname
+                    - priority
+                    - state
+                    - type
+                    - gotopriorityexpression
+                    - invoke
+                    - labeltype
+                    - labelname
+                    
+
+    auditsyslogpolicy_bindings:
+        desription: 
+        suboptions:
+            mode:
+                description:
+                    - If mode is C(bind):
+                    - Any bindings in the attributes list that do not exist will be created on the target Netscaler.
+                    - Existing bindings that are not on the attributes list remain unaffected.
+                    - If mode is C(unbind):
+                    - Any bindings defined in the attributes list that also exist on the target Netscaler will be removed.
+                    - Existing bindings that are not on the attributes list remain unaffected.
+                choices:
+                    - bind
+                    - unbind
+            attributes:
+                description: 
+                    - List of the attributes dictionaries for the bindings.
+                    - Valid attribute keys:
+                    - policyname
+                    - priority
+                    - state
+                    - type
+                    - gotopriorityexpression
+                    - invoke
+                    - labeltype
+                    - labelname
+                    
+
+
+extends_documentation_fragment: netscaler
+'''
+
+EXAMPLES = '''
+'''
+
+RETURN = '''
+loglines:
+    description: list of logged messages by the module
+    returned: always
+    type: list
+    sample: ['message 1', 'message 2']
+
+msg:
+    description: Message detailing the failure reason
+    returned: failure
+    type: str
+    sample: "Action does not exist"
+'''
+
+import copy
+
+from ansible.module_utils.basic import AnsibleModule
+from ansible.module_utils.network.netscaler.netscaler import NitroResourceConfig, NitroException, netscaler_common_arguments, log, loglines
+
+
+class ModuleExecutor(object):
+    
+    def __init__(self, module):
+        self.module = module
+        self.main_nitro_class = ''
+
+        # Dictionary containing attribute information
+        # for each NITRO object utilized by this module
+        self.attribute_config = {
+            
+            'appfwpolicy_bindings': {
+                'attributes_list': [
+                    
+                    'policyname',
+                    'priority',
+                    'gotopriorityexpression',
+                    'invoke',
+                    'state',
+                    'labeltype',
+                    'labelname',
+                    'type',
+                    'globalbindtype',
+                ],
+                'transforms': {
+                    
+                    'state': lambda v: v.upper(),
+                },
+                'get_id_attributes': [
+                    
+                    'type',
+                ],
+                'delete_id_attributes': [
+                    
+                    'policyname',
+                    'priority',
+                    'type',
+                ],
+            },
+            
+            'auditnslogpolicy_bindings': {
+                'attributes_list': [
+                    
+                    'policyname',
+                    'priority',
+                    'state',
+                    'type',
+                    'gotopriorityexpression',
+                    'invoke',
+                    'labeltype',
+                    'labelname',
+                ],
+                'transforms': {
+                    
+                    'state': lambda v: v.upper(),
+                },
+                'get_id_attributes': [
+                    
+                    'type',
+                ],
+                'delete_id_attributes': [
+                    
+                    'policyname',
+                    'priority',
+                    'type',
+                ],
+            },
+            
+            'auditsyslogpolicy_bindings': {
+                'attributes_list': [
+                    
+                    'policyname',
+                    'priority',
+                    'state',
+                    'type',
+                    'gotopriorityexpression',
+                    'invoke',
+                    'labeltype',
+                    'labelname',
+                ],
+                'transforms': {
+                    
+                    'state': lambda v: v.upper(),
+                },
+                'get_id_attributes': [
+                    
+                    'type',
+                ],
+                'delete_id_attributes': [
+                    
+                    'policyname',
+                    'priority',
+                    'type',
+                ],
+            },
+            
+
+        }
+
+        self.module_result = dict(
+            changed=False,
+            failed=False,
+            loglines=loglines,
+        )
+
+
+    def sync_binding_with_data(self, data):
+
+        binding_key = data['binding_key']
+        binding_object = data['binding_object']
+
+        if self.module.params.get(binding_key) is None:
+            return
+
+        log('ModuleExecutor syncing binding %s' % binding_key)
+
+        mode = self.module.params[binding_key]['mode']
+
+        # Make a list of config objects for configured bindings
+        configured_bindings = []
+        for bind_values in self.module.params[binding_key]['attributes']:
+            all_bind_values = copy.deepcopy(bind_values)
+            configured_binding = NitroResourceConfig(
+                module=self.module,
+                resource=binding_object,
+                attribute_values_dict=all_bind_values,
+                attributes_list=self.attribute_config[binding_key]['attributes_list'],
+                transforms=self.attribute_config[binding_key]['transforms'],
+            )
+
+            configured_bindings.append(configured_binding)
+
+
+        if mode == 'bind':
+            for configured_binding in configured_bindings:
+                self.module_result['changed'] = True
+                try:
+                    configured_binding.create()
+                except NitroException as e:
+                    if e.errorcode != 273:
+                        raise
+
+        elif mode == 'unbind':
+            for configured_binding in configured_bindings:
+                self.module_result['changed'] = True
+                try:
+                    configured_binding.delete(delete_id_attributes=self.attribute_config[binding_key]['delete_id_attributes'])
+                except NitroException as e:
+                    # Handle exceptions when trying to unbind objects that are not bound
+                    # Every binding has its own errorcode
+                    if binding_object == 'appfwglobal_appfwpolicy_binding':
+                        if e.errorcode == 3093:
+                            log('Ignoring nitro_errocode 3093 for appfwglobal_appfwpolicy_binding')
+                        else:
+                            raise
+
+    def sync_bindings(self):
+        log('ModuleExecutor.sync_bindings()')
+        
+        self.sync_appfwpolicy_bindings()
+        
+        self.sync_auditnslogpolicy_bindings()
+        
+        self.sync_auditsyslogpolicy_bindings()
+        
+
+
+        
+        
+
+    def sync_appfwpolicy_bindings(self):
+        self.sync_binding_with_data({'binding_key': 'appfwpolicy_bindings', 'doc_list': ['policyname', 'priority', 'gotopriorityexpression', 'invoke', 'state', 'labeltype', 'labelname', 'type', 'globalbindtype'], 'binding_object': 'appfwglobal_appfwpolicy_binding', 'attributes_config_list': {'get_id_attributes': ['type'], 'attributes': ['policyname', 'priority', 'gotopriorityexpression', 'invoke', 'state', 'labeltype', 'labelname', 'type', 'globalbindtype'], 'resource_name': 'appfwpolicy_bindings', 'delete_id_attributes': ['policyname', 'priority', 'type'], 'transforms': {'state': 'lambda v: v.upper()'}}})
+        
+        
+
+    def sync_auditnslogpolicy_bindings(self):
+        self.sync_binding_with_data({'binding_key': 'auditnslogpolicy_bindings', 'doc_list': ['policyname', 'priority', 'state', 'type', 'gotopriorityexpression', 'invoke', 'labeltype', 'labelname'], 'binding_object': 'appfwglobal_auditnslogpolicy_binding', 'attributes_config_list': {'get_id_attributes': ['type'], 'attributes': ['policyname', 'priority', 'state', 'type', 'gotopriorityexpression', 'invoke', 'labeltype', 'labelname'], 'resource_name': 'auditnslogpolicy_bindings', 'delete_id_attributes': ['policyname', 'priority', 'type'], 'transforms': {'state': 'lambda v: v.upper()'}}})
+        
+        
+
+    def sync_auditsyslogpolicy_bindings(self):
+        self.sync_binding_with_data({'binding_key': 'auditsyslogpolicy_bindings', 'doc_list': ['policyname', 'priority', 'state', 'type', 'gotopriorityexpression', 'invoke', 'labeltype', 'labelname'], 'binding_object': 'appfwglobal_auditsyslogpolicy_binding', 'attributes_config_list': {'get_id_attributes': ['type'], 'attributes': ['policyname', 'priority', 'state', 'type', 'gotopriorityexpression', 'invoke', 'labeltype', 'labelname'], 'resource_name': 'auditsyslogpolicy_bindings', 'delete_id_attributes': ['policyname', 'priority', 'type'], 'transforms': {'state': 'lambda v: v.upper()'}}})
+        
+
+    def main(self):
+        try:
+
+            if self.module.params['state'] == 'present':
+                self.sync_bindings()
+            elif self.module.params['state'] == 'absent':
+                log('state "absent" is a noop for this module')
+
+            self.module.exit_json(**self.module_result)
+
+        except NitroException as e:
+            msg = "nitro exception errorcode=%s, message=%s, severity=%s" % (str(e.errorcode), e.message, e.severity)
+            self.module.fail_json(msg=msg, **self.module_result)
+        except Exception as e:
+            msg = 'Exception %s: %s' % (type(e), str(e))
+            self.module.fail_json(msg=msg, **self.module_result)
+
+
+
+def main():
+
+
+    argument_spec = dict()
+
+    module_specific_arguments = dict(
+        
+        
+        appfwpolicy_bindings=dict(type='dict'),
+        auditnslogpolicy_bindings=dict(type='dict'),
+        auditsyslogpolicy_bindings=dict(type='dict'),
+
+    )
+
+
+    argument_spec.update(netscaler_common_arguments)
+    argument_spec.update(module_specific_arguments)
+
+
+    module = AnsibleModule(
+        argument_spec=argument_spec,
+        supports_check_mode=False,
+    )
+
+    executor = ModuleExecutor(module=module)
+    executor.main()
+
+
+if __name__ == '__main__':
+    main()
---
+++ utils/source/generators/generate_netscaler_appfwglobal_bindings.py
@@ -0,0 +1,97 @@
+import os
+import copy
+import json
+from jinja2 import Environment, FileSystemLoader
+
+from helpers import calculate_transforms_for_attribute, calculate_doc_list, calculate_attributes_config_dict
+
+
+HERE = os.path.abspath(os.path.dirname(__file__))
+
+def populate_template(template_dir, **kwargs):
+    env = Environment(
+        # keep python templates valid python
+        block_start_string='#{%',
+        loader=FileSystemLoader(template_dir),
+        # trim_blocks=True,
+        # lstrip_blocks=True,
+    )
+
+    template = env.get_template('netscaler_appfwglobal_bindings.template')
+    stream = template.stream(**kwargs)
+
+    #output_file = 'netscaler_appfw_profile.py'
+    output_file = os.path.join(HERE, '..', '..', '..', 'ansible-modules', 'netscaler_appfw_global_bindings.py')
+    stream.dump(
+        output_file,
+        encoding='utf-8'
+    )
+
+def main():
+
+    # Find template dir
+    template_dir = os.path.join(HERE, '..', 'templates', 'appfw')
+    if not os.path.exists(template_dir):
+        raise Exception('Cannot find template dir')
+
+    # Find nscli json data dir
+    json_dir = os.path.join(HERE, '..', 'nitro_api_defines/kamet/appfw')
+    if not os.path.exists(json_dir):
+        raise Exception('Cannot find json dir')
+
+    attributes_config_list = []
+
+    # Add the bindings objects
+    bindings = []
+
+    def append_bindings(json_file, binding_dict):
+        with open(json_file, 'r') as fh:
+            attributes = json.load(fh)
+
+        key = binding_dict['binding_key']
+        binding_dict.update({
+            'attributes_config_list': calculate_attributes_config_dict(key, attributes),
+        })
+        doc_list = copy.deepcopy(binding_dict['attributes_config_list']['attributes'])
+        binding_dict['doc_list'] = doc_list
+        bindings.append(binding_dict)
+        attributes_config_list.append(binding_dict['attributes_config_list'])
+
+    # FIXME appfw policy bindings seem not to work on the NITRO level
+    append_bindings(
+        json_file=os.path.join(json_dir, 'appfwglobal_appfwpolicy_binding.json'),
+        binding_dict={
+            'binding_key': 'appfwpolicy_bindings',
+            'binding_object': 'appfwglobal_appfwpolicy_binding',
+            },
+    )
+
+    append_bindings(
+        json_file=os.path.join(json_dir, 'appfwglobal_auditnslogpolicy_binding.json'),
+        binding_dict={
+            'binding_key': 'auditnslogpolicy_bindings',
+            'binding_object': 'appfwglobal_auditnslogpolicy_binding',
+            },
+    )
+
+    append_bindings(
+        json_file=os.path.join(json_dir, 'appfwglobal_auditsyslogpolicy_binding.json'),
+        binding_dict={
+            'binding_key': 'auditsyslogpolicy_bindings',
+            'binding_object': 'appfwglobal_auditsyslogpolicy_binding',
+            },
+    )
+
+    print(json.dumps(attributes_config_list, indent=4))
+
+    # Populate the template
+    populate_template(
+        template_dir,
+        attributes_config_list=attributes_config_list,
+        bindings=bindings,
+    )
+
+
+
+if __name__ == '__main__':
+    main()
