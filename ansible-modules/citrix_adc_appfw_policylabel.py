cf7eae0ce943ea04e920716f12252963dbc9a28d
George Nikolopoulos <giorgos.nikolopoulos@citrix.com>
2020-01-09 18:32:24 +0200

    Fix pep8 errors for citrix_adc_appfw_policylabel

--- ansible-modules/citrix_adc_appfw_policylabel.py
+++ ansible-modules/citrix_adc_appfw_policylabel.py
@@ -12,8 +12,6 @@ ANSIBLE_METADATA = {'metadata_version': '1.1',
                     'status': ['preview'],
                     'supported_by': 'community'}
 
-
-
 DOCUMENTATION = '''
 ---
 module: citrix_adc_appfw_policylabel
@@ -34,8 +32,13 @@ options:
     labelname:
         description:
             - >-
-                Name of the policy label to invoke if the current policy evaluates to TRUE, the invoke parameter is
-                and Label Type is set to Policy Label.
+                Name for the policy label. Must begin with a letter, number, or the underscore character (_), and
+                contain only letters, numbers, and the hyphen (-), period (.) pound (#), space ( ), at (@), equals
+                colon (:), and underscore characters. Can be changed after the policy label is created.
+            - "The following requirement applies only to the Citrix ADC CLI:"
+            - >-
+                If the name includes one or more spaces, enclose the name in double or single quotation marks (for
+                "my policy label" or 'my policy label').
         type: str
 
     policylabeltype:
@@ -104,27 +107,20 @@ class ModuleExecutor(object):
         # Dictionary containing attribute information
         # for each NITRO object utilized by this module
         self.attibute_config = {
-            
             'appfwpolicylabel': {
                 'attributes_list': [
-                    
                     'labelname',
                     'policylabeltype',
-                    'newname',
                 ],
                 'transforms': {
-                    
                 },
                 'get_id_attributes': [
-                    
                     'labelname',
                 ],
                 'delete_id_attributes': [
-                    
                     'labelname',
                 ],
             },
-            
 
         }
 
@@ -133,7 +129,6 @@ class ModuleExecutor(object):
             failed=False,
             loglines=loglines,
         )
-
 
     def update_or_create(self):
         # Check if main object exists
@@ -164,9 +159,6 @@ class ModuleExecutor(object):
                 self.module_result['changed'] = True
                 if not self.module.check_mode:
                     config.update(id_attribute='name')
-
-
-    
 
     def delete(self):
         # Check if main object exists
@@ -209,31 +201,24 @@ class ModuleExecutor(object):
             self.module.fail_json(msg=msg, **self.module_result)
 
 
-
 def main():
-
 
     argument_spec = dict()
 
     module_specific_arguments = dict(
-        
         labelname=dict(type='str'),
-        
         policylabeltype=dict(
             type='str',
             choices=[
                 'http_req',
             ]
         ),
-        
         appfwpolicy_csvserver_bindings=dict(type='dict'),
         denyurl_bindings=dict(type='dict'),
     )
 
-
     argument_spec.update(netscaler_common_arguments)
     argument_spec.update(module_specific_arguments)
-
 
     module = AnsibleModule(
         argument_spec=argument_spec,
