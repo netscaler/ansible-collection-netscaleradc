d63e0565acfe73d4521fc89bc349e98cf7f6918d
George Nikolopoulos <giorgos.nikolopoulos@citrix.com>
2020-10-19 14:35:49 +0300

    Create citrixadc_modules collection

---
+++ ansible-collections/citrixadc_modules/CHANGELOG.rst
@@ -0,0 +1,44 @@
+======================================
+Citrix.Citrixadc_Modules Release Notes
+======================================
+
+.. contents:: Topics
+
+
+v1.0.0
+======
+
+New Modules
+-----------
+
+- citrix.citrixadc_modules.citrix_adc_appfw_confidfield - Configuration for configured confidential form fields resource.
+- citrix.citrixadc_modules.citrix_adc_appfw_fieldtype - Configuration for application firewall form field type resource.
+- citrix.citrixadc_modules.citrix_adc_appfw_global_bindings - Define global bindings for AppFW
+- citrix.citrixadc_modules.citrix_adc_appfw_htmlerrorpage - Configuration for configured confidential form fields resource.
+- citrix.citrixadc_modules.citrix_adc_appfw_jsoncontenttype - Configuration for JSON content type resource.
+- citrix.citrixadc_modules.citrix_adc_appfw_learningsettings - Configuration for learning settings resource.
+- citrix.citrixadc_modules.citrix_adc_appfw_policy - Manage Citrix ADC Web Application Firewall policies.
+- citrix.citrixadc_modules.citrix_adc_appfw_policylabel - Manage Citrix ADC Web Application Firewall policy labels.
+- citrix.citrixadc_modules.citrix_adc_appfw_profile - Manage Citrix ADC Web Application Firewall profiles.
+- citrix.citrixadc_modules.citrix_adc_appfw_settings - Manage Citrix ADC Web Application Firewall settings.
+- citrix.citrixadc_modules.citrix_adc_appfw_signatures - Configuration for configured confidential form fields resource.
+- citrix.citrixadc_modules.citrix_adc_appfw_wsdl - Configuration for configured confidential form fields resource.
+- citrix.citrixadc_modules.citrix_adc_appfw_xmlcontenttype - Configuration for XML Content type resource.
+- citrix.citrixadc_modules.citrix_adc_appfw_xmlerrorpage - Configuration for configured confidential form fields resource.
+- citrix.citrixadc_modules.citrix_adc_appfw_xmlschema - Configuration for configured confidential form fields resource.
+- citrix.citrixadc_modules.citrix_adc_cs_action - Manage content switching actions
+- citrix.citrixadc_modules.citrix_adc_cs_policy - Manage content switching policy
+- citrix.citrixadc_modules.citrix_adc_cs_vserver - Manage content switching vserver
+- citrix.citrixadc_modules.citrix_adc_gslb_service - Manage gslb service entities in Netscaler.
+- citrix.citrixadc_modules.citrix_adc_gslb_site - Manage gslb site entities in Netscaler.
+- citrix.citrixadc_modules.citrix_adc_gslb_vserver - Configure gslb vserver entities in Netscaler.
+- citrix.citrixadc_modules.citrix_adc_lb_monitor - Manage load balancing monitors
+- citrix.citrixadc_modules.citrix_adc_lb_vserver - Manage load balancing vserver configuration
+- citrix.citrixadc_modules.citrix_adc_nitro_request - Issue Nitro API requests to a Netscaler instance.
+- citrix.citrixadc_modules.citrix_adc_nitro_resource - Manage NITRO resources
+- citrix.citrixadc_modules.citrix_adc_password_reset - Perform default password reset.
+- citrix.citrixadc_modules.citrix_adc_save_config - Save Netscaler configuration.
+- citrix.citrixadc_modules.citrix_adc_server - Manage server configuration
+- citrix.citrixadc_modules.citrix_adc_service - Manage service configuration in Netscaler
+- citrix.citrixadc_modules.citrix_adc_servicegroup - Manage service group configuration in Netscaler
+- citrix.citrixadc_modules.citrix_adc_ssl_certkey - Manage ssl cerificate keys.
---
+++ ansible-collections/citrixadc_modules/plugins/modules/citrix_adc_appfw_wsdl.py
@@ -0,0 +1,227 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+#  Copyright (c) 2018 Citrix Systems
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+from __future__ import absolute_import, division, print_function
+__metaclass__ = type
+
+
+ANSIBLE_METADATA = {'metadata_version': '1.1',
+                    'status': ['preview'],
+                    'supported_by': 'community'}
+
+DOCUMENTATION = '''
+---
+module: citrix_adc_appfw_wsdl
+short_description: Configuration for configured confidential form fields resource.
+description: Configuration for configured confidential form fields resource.
+
+version_added: "1.0.0"
+
+author:
+    - George Nikolopoulos (@giorgos-nikolopoulos)
+    - Sumanth Lingappa (@sumanth-lingappa)
+
+options:
+
+    name:
+        description:
+            - "Name of the WSDL file to remove."
+            - "Minimum length =  1"
+            - "Maximum length =  31"
+        type: str
+
+    src:
+        description:
+            - "URL (protocol, host, path, and name) of the WSDL file to be imported is stored."
+            - >-
+                NOTE: The import fails if the object to be imported is on an HTTPS server that requires client
+                authentication for access.
+            - "Minimum length =  1"
+            - "Maximum length =  2047"
+        type: str
+
+    comment:
+        description:
+            - "Any comments to preserve information about the WSDL."
+            - "Maximum length =  128"
+        type: str
+
+    overwrite:
+        description:
+            - "Overwrite any existing WSDL of the same name."
+        type: bool
+
+
+extends_documentation_fragment: citrix.citrixadc_modules.citrixadc
+'''
+
+EXAMPLES = '''
+- hosts: citrix_adc
+
+  gather_facts: False
+  tasks:
+    - name: Setup confidential field id
+      delegate_to: localhost
+      citrix_adc_appfw_wsdl:
+        nitro_user: nsroot
+        nitro_pass: nsroot
+        nsip: 192.168.1.2
+        state: present
+        fieldname: wsdl_integration_test
+        url: 'HTTP.REQ.HOSTNAME.DOMAIN.EQ("blog.example.com")'
+        isregex: REGEX
+        comment: 'conf id field comment'
+'''
+
+RETURN = '''
+loglines:
+    description: list of logged messages by the module
+    returned: always
+    type: list
+    sample: ['message 1', 'message 2']
+
+msg:
+    description: Message detailing the failure reason
+    returned: failure
+    type: str
+    sample: "Action does not exist"
+'''
+
+import copy
+
+from ansible.module_utils.basic import AnsibleModule
+from ansible_collections.citrix.citrixadc_modules.plugins.module_utils.citrix_adc import (
+    NitroResourceConfig,
+    NitroException,
+    netscaler_common_arguments,
+    log,
+    loglines
+)
+
+
+class ModuleExecutor(object):
+
+    def __init__(self, module):
+        self.module = module
+        self.main_nitro_class = 'appfwwsdl'
+
+        # Dictionary containing attribute information
+        # for each NITRO object utilized by this module
+        self.attibute_config = {
+            'appfwwsdl': {
+                'attributes_list': [
+                    'name',
+                    'src',
+                    'comment',
+                    'overwrite',
+                ],
+                'transforms': {
+                },
+                'get_id_attributes': [
+                    'name',
+                ],
+                'delete_id_attributes': [
+                    'name',
+                ],
+            },
+
+        }
+
+        self.module_result = dict(
+            changed=False,
+            failed=False,
+            loglines=loglines,
+        )
+
+    def main_object_exists(self, config):
+        try:
+            main_object_exists = config.exists(get_id_attributes=self.attibute_config[self.main_nitro_class]['get_id_attributes'])
+        except NitroException as e:
+            if e.errorcode == 3377 or e.errorcode == 3187:
+                # errorcode 3377: "WSDL does not exist" in 12.1
+                # errorcode 3187: "Imported file does not exist" in NS11.1 and NS12.0
+                return False
+            else:
+                raise
+
+        return main_object_exists
+
+    def get_main_config(self):
+        manipulated_values_dict = copy.deepcopy(self.module.params)
+
+        config = NitroResourceConfig(
+            module=self.module,
+            resource=self.main_nitro_class,
+            attribute_values_dict=manipulated_values_dict,
+            attributes_list=self.attibute_config[self.main_nitro_class]['attributes_list'],
+            transforms=self.attibute_config[self.main_nitro_class]['transforms'],
+        )
+
+        return config
+
+    def import_and_update(self):
+        # check if main object exists
+        config = self.get_main_config()
+
+        if not self.main_object_exists(config):
+            self.module_result['changed'] = True
+            if not self.module.check_mode:
+                config.import_object()
+                # config.update_object() # There's no UPDATE operation in XMLSCHEMA unlike HTMLERRORCODE and XMLERRORCODE
+
+    def delete(self):
+        # Check if main object exists
+        config = self.get_main_config()
+
+        if self.main_object_exists(config):
+            self.module_result['changed'] = True
+            if not self.module.check_mode:
+                config.delete(delete_id_attributes=self.attibute_config[self.main_nitro_class]['delete_id_attributes'])
+
+    def main(self):
+        try:
+
+            if self.module.params['state'] == 'present':
+                self.import_and_update()
+            elif self.module.params['state'] == 'absent':
+                self.delete()
+
+            self.module.exit_json(**self.module_result)
+
+        except NitroException as e:
+            msg = "nitro exception errorcode=%s, message=%s, severity=%s" % (str(e.errorcode), e.message, e.severity)
+            self.module.fail_json(msg=msg, **self.module_result)
+        except Exception as e:
+            msg = 'Exception %s: %s' % (type(e), str(e))
+            self.module.fail_json(msg=msg, **self.module_result)
+
+
+def main():
+
+    argument_spec = dict()
+
+    module_specific_arguments = dict(
+        name=dict(type='str'),
+        src=dict(type='str'),
+        comment=dict(type='str'),
+        overwrite=dict(type='bool'),
+
+    )
+
+    argument_spec.update(netscaler_common_arguments)
+    argument_spec.update(module_specific_arguments)
+
+    module = AnsibleModule(
+        argument_spec=argument_spec,
+        supports_check_mode=True,
+    )
+
+    executor = ModuleExecutor(module=module)
+    executor.main()
+
+
+if __name__ == '__main__':
+    main()
