9248dc5084f65c970742d1429c29efda64050785
Sumanth Lingappa <sumanth.lingappa@cloud.com>
2024-02-07 14:49:42 +0530

    added module name in the TRACE logs
    Signed-off-by: Sumanth Lingappa <sumanth.lingappa@cloud.com>

--- plugins/module_utils/decorators.py
+++ plugins/module_utils/decorators.py
@@ -15,9 +15,19 @@ from .logger import log
 def trace(func):
     @functools.wraps(func)
     def wrapper(*args, **kwargs):
-        log("TRACE: ENTRY: {}() called with {}, {}".format(func.__name__, args, kwargs))
+        module = func.__module__
+        module = module.split(".")[-1]
+        log(
+            "TRACE: ENTRY: {}.{}() called with {}, {}".format(
+                module, func.__name__, args, kwargs
+            )
+        )
         original_result = func(*args, **kwargs)
-        log("TRACE: EXIT: {}() returned {}".format(func.__name__, original_result))
+        log(
+            "TRACE: EXIT: {}.{}() returned {}".format(
+                module, func.__name__, original_result
+            )
+        )
         return original_result
 
     return wrapper
